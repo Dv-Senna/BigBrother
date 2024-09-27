fdcba8c6da000e7b5ff84bad9d474b1df57facd9
Dv-Senna <dvsenna64@gmail.com>
2024-09-27 21:13:53 +0200

    Add SceneNames to avoid typos

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from sceneManager import *
 from scenes.doorScene import *
 
 
+class SceneNames:
+	DOOR = "doorScene"
+	MAIN_MENU = "mainMenuScene"
+
+# to check if a scene is a doorScene, just use `if SceneManager.getCurrentSceneName()[:-1] == SceneNames.DOOR`
+
 def main():
 	doorScenes = [
 		DoorScene(pg.image.load("res/spritesheet.png"))
 	]
 
 	for i in range(0, len(doorScenes)):
-		SceneManager.addScene(f"doorScene{i}", doorScenes[i])
-	SceneManager.setCurrentScene("doorScene0")
+		SceneManager.addScene(f"{SceneNames.DOOR}{i}", doorScenes[i])
+	SceneManager.setCurrentScene(f"{SceneNames.DOOR}0")
 
 	fpsClock = pg.time.Clock()
 	window = pg.display.set_mode((16*70, 9*70))
