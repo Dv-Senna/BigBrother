d8c01fe1a81492209c20b4151b9ff0137720848c
Samuel Eklu <samuel.eklu@epfl.ch>
2024-09-28 00:29:41 +0200

    2nd try with callbacks

--- eventHandler.py
+++ eventHandler.py
@@ -1,18 +1,22 @@
 import pygame as pg
+from event_manager import EventManager
 
 class EventHandler:
-    def __init__(self, window, backgrounds, arrow_left, arrow_right):
+    def __init__(self, window, backgrounds, arrow_left, arrow_right, event_manager):
         self.window = window
         self.backgrounds = backgrounds
-        self.current_background_index = 0  # Index de l'image de fond actuelle
+        self.current_background_index = 0
 
         # Taille et position des boutons (flèches)
         self.arrow_left_rect = pg.Rect(20, (self.window.get_height() // 2) - 50, 50, 100)  # Taille 50x100
-        self.arrow_right_rect = pg.Rect(self.window.get_width() - 70, (self.window.get_height() // 2) - 50, 50, 100)  # Taille 50x100
+        self.arrow_right_rect = pg.Rect(self.window.get_width() - 70, (self.window.get_height() // 2) - 50, 50, 100)
 
-        # Redimensionner les images pour qu'elles correspondent à la taille des hitboxes
+        # Redimensionner les images des flèches
         self.arrow_left = pg.transform.scale(arrow_left, (self.arrow_left_rect.width, self.arrow_left_rect.height))
         self.arrow_right = pg.transform.scale(arrow_right, (self.arrow_right_rect.width, self.arrow_right_rect.height))
+
+        # Gestionnaire d'événements
+        self.event_manager = event_manager
 
     def handle_events(self):
         for event in pg.event.get():
@@ -21,10 +25,23 @@ class EventHandler:
                 exit()
             elif event.type == pg.MOUSEBUTTONDOWN:  # Clic de souris
                 if self.arrow_left_rect.collidepoint(event.pos):  # Clic sur la flèche gauche
-                    self.change_background(-1)
+                    self.event_manager.trigger_event("arrow_left_clicked")  # Déclenche l'événement
                 elif self.arrow_right_rect.collidepoint(event.pos):  # Clic sur la flèche droite
-                    self.change_background(1)
+                    self.event_manager.trigger_event("arrow_right_clicked")  # Déclenche l'événement
 
+    def display_current_screen(self):
+        """Affiche le fond actuel, les flèches, et les hitboxes sur la fenêtre."""
+        current_background = self.backgrounds[self.current_background_index]
+        self.window.blit(current_background, (0, 0))
+
+        # Afficher les flèches redimensionnées
+        self.window.blit(self.arrow_left, self.arrow_left_rect.topleft)
+        self.window.blit(self.arrow_right, self.arrow_right_rect.topleft)
+
+        # Dessiner les bordures (hitboxes) autour des flèches
+        pg.draw.rect(self.window, (0, 0, 0), self.arrow_left_rect, 2)
+        pg.draw.rect(self.window, (0, 0, 0), self.arrow_right_rect, 2)
+    
     def change_background(self, direction):
         """Change le fond en fonction de la direction (-1 pour gauche, 1 pour droite)."""
         self.current_background_index += direction
@@ -34,17 +51,3 @@ class EventHandler:
             self.current_background_index = len(self.backgrounds) - 1
         elif self.current_background_index >= len(self.backgrounds):
             self.current_background_index = 0
-
-    def display_current_screen(self):
-        """Affiche le fond actuel, les flèches, et les hitboxes sur la fenêtre."""
-        # Afficher le fond actuel
-        current_background = self.backgrounds[self.current_background_index]
-        self.window.blit(current_background, (0, 0))
-
-        # Afficher les flèches redimensionnées
-        self.window.blit(self.arrow_left, self.arrow_left_rect.topleft)
-        self.window.blit(self.arrow_right, self.arrow_right_rect.topleft)
-
-        # Dessiner les bordures (hitboxes) autour des flèches
-        pg.draw.rect(self.window, (0, 0, 0), self.arrow_left_rect, 2)  # Dessiner une bordure noire de 2 pixels
-        pg.draw.rect(self.window, (0, 0, 0), self.arrow_right_rect, 2)  # Dessiner une bordure noire de 2 pixels
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame as pg
 from eventHandler import EventHandler
-
+from event_manager import EventManager
 
 def main():
     pg.init()  # Initialisation de Pygame
     fpsClock = pg.time.Clock()
-    window = pg.display.set_mode((16*70, 9*70))  # Taille de la fenêtre
+    window = pg.display.set_mode((16*70, 9*70))
     pg.display.set_caption("BigBrother")
 
     # Charger les images de fond et les flèches
@@ -13,14 +13,30 @@ def main():
     arrow_left = pg.image.load("img/utils/arrow_left.png")
     arrow_right = pg.image.load("img/utils/arrow_right.png")
 
-    # Initialisation du gestionnaire d'événements
-    event_handler = EventHandler(window, backgrounds, arrow_left, arrow_right)
+    # Créer le gestionnaire d'événements
+    event_manager = EventManager()
+
+    # Fonction de callback pour changer le fond d'écran
+    def on_arrow_left_click():
+        event_handler.change_background(-1)
+
+    def on_arrow_right_click():
+        event_handler.change_background(1)
+
+    # Enregistrer les événements et les callbacks
+    event_manager.register_event("arrow_left_clicked")
+    event_manager.register_event("arrow_right_clicked")
+    event_manager.register_callback("arrow_left_clicked", on_arrow_left_click)
+    event_manager.register_callback("arrow_right_clicked", on_arrow_right_click)
+
+    # Initialiser l'event handler
+    event_handler = EventHandler(window, backgrounds, arrow_left, arrow_right, event_manager)
 
     while True:
         # Gestion des événements par le fichier eventHandler.py
         event_handler.handle_events()
 
-        # Mise à jour de l'affichage dans la fenêtre
+        # Mise à jour de l'affichage
         window.fill((255, 255, 255))  # Fond blanc
         event_handler.display_current_screen()  # Afficher l'écran actuel (fond et flèches)
 
