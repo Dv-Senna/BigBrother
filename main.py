c2d629c0df7d38a8faa16e9670c59adc9c6c8ec8
Morgan-DD <111990474+Morgan-DD@users.noreply.github.com>
2024-09-28 06:07:25 +0200

    working on visibility toggling

--- main.py
+++ main.py
@@ -15,26 +15,21 @@ def main():
 	pg.font.init()
 	window = pg.display.set_mode((16*70, 9*70))
 	pg.display.set_caption("BigBrother")
-	IMG = pg.image.load("test.png")
-	_dialogManagertest = DialogManager(window, IMG, 25)
+	background_img = pg.image.load("test.png")
+
 	testText = """"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi archonsectetur,re magnam aliqutationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur"
 	"""
-	_dialogManagertest.DisplayText(testText)
-	# _dialogManagertest.UpdateText("TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST")
-	# _dialogManagertest.ShowDialog()
-	_dialogManagertest.HideDialog()
-	_dialogManagertest.ShowDialog()
+	dialog_manager = DialogManager(window, background_img, 20,  testText)
+	dialog_manager.display()
+	dialog_manager.hide()
+	print(dialog_manager.getVisiblity())
+	# dialog_manager.set_text(testText)
+	# dialog_manager.toggle_visibility()
+
 	while True:
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				return
-		# update section
-		# SceneManager.getCurrentScene().updateCallback()
-		# draw section
-		# window.fill((0, 0, 0))
-
-		# SceneManager.getCurrentScene().renderCallback()
-
 		pg.display.update()
 		fpsClock.tick(60)
 
