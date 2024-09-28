d2f29c6d8d50e64251b0d46934aee969a50622e8
Dv-Senna <dvsenna64@gmail.com>
2024-09-28 10:37:28 +0200

    Add slide transition

--- sceneManager.py
+++ sceneManager.py
@@ -25,6 +25,16 @@ class SceneManager:
 	scenes: dict[str, Scene] = {}
 	transitionTimer: int = 0
 	blackScreenOpacity: int = 0
+
+	currentTransition = ""
+	oldCurrentScene: str = ""
+	oldDoorScreen: pg.Surface = pg.Surface((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
+	newDoorScreen: pg.Surface = pg.Surface((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
+	oldDoorScreenRect: pg.Rect = pg.Rect(0, 0, config.WINDOW_WIDTH, config.WINDOW_HEIGHT)
+	newDoorScreenRect: pg.Rect = pg.Rect(0, 0, config.WINDOW_WIDTH, config.WINDOW_HEIGHT)
+	newDoorIndex: int = -1
+	oldDoorIndex: int = -1
+
 	
 
 	@staticmethod
@@ -37,6 +47,16 @@ class SceneManager:
 			return
 
 		SceneManager.targetScene = sceneName
+		if SceneManager.currentScene[:-1] == "doorScene":
+			SceneManager.oldCurrentScene = SceneManager.currentScene
+			SceneManager.currentScene = SceneManager.targetScene
+			SceneManager.currentTransition = "slide"
+
+			SceneManager.oldDoorIndex = int(SceneManager.oldCurrentScene[-1])
+			SceneManager.newDoorIndex = int(SceneManager.currentScene[-1])
+
+		else:
+			SceneManager.currentTransition = "black"
 
 	@staticmethod
 	def update(dt: int):
@@ -50,30 +70,58 @@ class SceneManager:
 			if SceneManager.currentScene != "":
 				SceneManager.scenes[SceneManager.currentScene].unmount()
 
-
 		SceneManager.transitionTimer += dt
-		if SceneManager.currentScene[:-1] == "doorScene":
+
+
+
+		if SceneManager.currentTransition == "slide":
+			SceneManager.oldDoorScreenRect.x = int(- SceneManager.transitionTimer / config.SLIDE_TRANSITION_TIME * config.WINDOW_WIDTH)
+			SceneManager.newDoorScreenRect.x = int(config.WINDOW_WIDTH - SceneManager.transitionTimer / config.SLIDE_TRANSITION_TIME * config.WINDOW_WIDTH)
+
+			if SceneManager.oldDoorIndex > SceneManager.newDoorIndex:
+				SceneManager.oldDoorScreenRect.x *= -1
+				SceneManager.newDoorScreenRect.x *= -1
+
+			if SceneManager.transitionTimer < config.SLIDE_TRANSITION_TIME:
+				return
+
+			SceneManager.targetScene = ""
+			SceneManager.scenes[SceneManager.currentScene].mount()
+			SceneManager.transitionTimer = 0
+			SceneManager.oldDoorScreenRect.x = 0
+			SceneManager.newDoorScreenRect.x = 0
+			SceneManager.currentTransition = ""
+
+		else:
 			if SceneManager.transitionTimer < config.BLACK_FADE_TRANSITION_TIME / 2:
 				SceneManager.blackScreenOpacity = SceneManager.transitionTimer / config.BLACK_FADE_TRANSITION_TIME * 255 * 2
 			else:
 				SceneManager.currentScene = SceneManager.targetScene
 				SceneManager.blackScreenOpacity = 255 - (SceneManager.transitionTimer - config.BLACK_FADE_TRANSITION_TIME / 2) / config.BLACK_FADE_TRANSITION_TIME * 255 * 2
 
-		else:
-			pass
+			if SceneManager.transitionTimer < config.BLACK_FADE_TRANSITION_TIME:
+				return
+			
+			SceneManager.currentScene = SceneManager.targetScene
+			SceneManager.targetScene = ""
+			SceneManager.scenes[SceneManager.currentScene].mount()
+			SceneManager.transitionTimer = 0
+			SceneManager.currentTransition = ""
 
-		if SceneManager.transitionTimer < config.BLACK_FADE_TRANSITION_TIME:
-			return
-		
-		SceneManager.currentScene = SceneManager.targetScene
-		SceneManager.targetScene = ""
-		SceneManager.scenes[SceneManager.currentScene].mount()
-		SceneManager.transitionTimer = 0
+
+
 
 	@staticmethod
 	def render(window: pg.Surface):
 		if SceneManager.currentScene != "":
-			SceneManager.scenes[SceneManager.currentScene].render(window)
+			if SceneManager.currentTransition == "slide":
+				SceneManager.scenes[SceneManager.oldCurrentScene].render(SceneManager.oldDoorScreen)
+				SceneManager.scenes[SceneManager.currentScene].render(SceneManager.newDoorScreen)
+				window.blit(SceneManager.oldDoorScreen, SceneManager.oldDoorScreenRect)
+				window.blit(SceneManager.newDoorScreen, SceneManager.newDoorScreenRect)
+
+			else:
+				SceneManager.scenes[SceneManager.currentScene].render(window)
 
 	@staticmethod
 	def getCurrentScene():
